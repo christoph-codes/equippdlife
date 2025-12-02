0123e896e8c90a7910f6052f3440de571c3a08eb
christoph-codes <tkcwebdev@gmail.com>
2025-12-02 10:51:32 -0800

    main: Add footer, new desert dark color, and set standard tab sizes for code formatting

--- src/app/devotions/layout.tsx
+++ src/app/devotions/layout.tsx
@@ -1,9 +1,11 @@
+import Footer from "@/components/Footer";
 import { Header } from "@/components/Header";
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<div className="flex flex-col min-h-screen">
 			<Header />
 			<div className="flex-1 flex flex-col overflow-y-auto">{children}</div>
+			<Footer />
 		</div>
 	);
 }
